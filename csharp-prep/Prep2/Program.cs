47fef11e45001f636ae51c08bfeb4b5a47411995
bmillettmedia <bryanmillett@cox.net>
2024-09-19 17:42:11 -0700

    practice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade:");
+        string value = Console.ReadLine();
+
+        int value = int.Parse(value);
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter ="A";
+
+        }
+       
+        else if (value >= 80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >= 60)
+        {
+            letter = "D";
+        }
+         else {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}");
+
+        if(value >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better Luck Next time!");
+        }
     }
 }
