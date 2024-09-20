09a6a75920c185340bea33f7f37435642ad073df
bmillettmedia <bryanmillett@cox.net>
2024-09-19 17:40:47 -0700

    practice stuff

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+    string FName= Console.ReadLine();
+    Console.WriteLine("What is your Last Name?");
+    string LName= Console.ReadLine();
+
+    Console.WriteLine($"Your name is {FName} {LName}.");
     }
+    
 }
