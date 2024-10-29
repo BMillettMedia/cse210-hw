5879aa97825ed14b18158eccbddde1ee451a6f88
bmillettmedia <bryanmillett@cox.net>
2024-10-29 16:13:43 -0700

    assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("");
+        Console.WriteLine(a1.GetSummary());
+
+        Math a2 = New Math("", "", "", "");
+        Console.WriteLine(a2.GetSummarry());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        Writing a3 = new Writing("", "", "");
+        Console.WriteLine(a3.GetSummarry());
+        Console.WriteLine(a3.GetWritingInfo());
+
+
     }
 }
